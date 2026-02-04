017b75d13f23c723768c874a8a5a791285589c80
anjilinux <anjireddy2024@gmail.com>
2026-02-04 23:30:58 +0530

    fix

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,5 @@
 import joblib
 
 def test_artifacts_exist():
-    assert joblib.load("artifacts/model.pkl")
-    assert joblib.load("artifacts/vectorizer.pkl")
+    assert joblib.load("model.pkl")
+    assert joblib.load("vectorizer.pkl")
